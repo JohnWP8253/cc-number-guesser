bd52a0bcb71386c143e26001d6299984fb14f5c9
John Pendergrass <john.pendergrass@me.com>
2020-08-04 14:34:05 -0500

    added code to compare guesses and name winner in script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNum = 1;
+
+const generateTarget = () => {
+   return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess ) => {
+    const humanDiff = Math.abs(targetGuess - humanGuess)
+    const computerDiff = Math.abs(targetGuess - computerGuess)
+    return humanDiff <= computerDiff;
+}
+
+const updateScore= winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+}
+
+const advanceRound = () => currentRoundNum++;
+
