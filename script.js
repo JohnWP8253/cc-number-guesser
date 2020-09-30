9c37e08bc65abbd6c4cd7f2f7757aec1c19bb588
John Pendergrass <john.pendergrass@me.com>
2020-09-30 10:22:20 -0500

    reformatted game.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+// create variables for scores and rounds
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNum = 1;
+
 
 const generateTarget = () => {
    return Math.floor(Math.random() * 10);
 }
 
+// create function with three params to compare guesses for the player, computer, and the target
 const compareGuesses = (humanGuess, computerGuess, targetGuess ) => {
     const humanDiff = Math.abs(targetGuess - humanGuess)
     const computerDiff = Math.abs(targetGuess - computerGuess)
     return humanDiff <= computerDiff;
 }
 
+// create function to decide winner
 const updateScore= winner => {
     if (winner === 'human') {
         humanScore++;
